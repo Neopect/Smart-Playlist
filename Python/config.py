57a7b448c5e71e2297508a297792a4108cc99ece
Neopect <tyler.moen@earthlink.net>
2021-09-23 09:53:07 -0500

    Config reading works

--- Python/config.py
+++ Python/config.py
@@ -4,7 +4,20 @@ confFile = []
 id = "null"
 secret = "null"
 gPlayl = []
-Playl = []
+playl = []
+
+# def init():
+#     global confFile
+#     global id
+#     global secret
+#     global gPlayl
+#     global playl
+
+# confFile = []
+# id = "null"
+# secret = "null"
+# gPlayl = []
+# playl = []
 
 def genConfig():
     print("Generating config folder...")
@@ -16,36 +29,92 @@ def genConfig():
     fw.write("[Client-ID]\nInsert Client ID\n\n")
     fw.write("[Client-Secret]\nInsert Client Secret\n\n")
     fw.write("# Playlists\n")
+    fw.write("# Example format conversion: https://open.spotify.com/playlist/2fhUKHlUCiCMdHKsFvg5Vf?si=91a60e2726964e1f --> spotify:playlist:2fhUKHlUCiCMdHKsFvg5Vf \n")
     fw.write("[Global-Playlists]\nInsert URI's for each global playlist\n\n")
     fw.write("[Playlists]\nInsert URI's for each users playlist\n\n")
     fw.close()
 
 def readConfig():
+    global id
+    global secret
+    global confFile
+    global gPlayl
+    global playl
     fw = open("config/config.conf", "r")
     for x in fw:
         confFile.append(x)
     print(confFile)
-    # Clean file
+    
+    # <<<<<<<<<<<<<<<<<< CLEAN FILE >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
     xdex = 0
     for x in confFile:
-        if x == '\n' or "#" in x:
-            print("removing x=" + str(x) +" xdex= " + str(xdex) + "\n")
-            # confFile.pop(xdex)
-            if xdex != 0:
-                 confFile.pop(xdex-1)
-            else:
-                confFile.pop(xdex)
-        print(confFile)
+        if "#" in x:
+            confFile.pop(xdex)
         xdex += 1
-    
-    # xdex = 0
-    # for x in confFile:
-    #     if x == "\n":
-    #         confFile.pop(xdex)
-    #     elif "\n" in x:
-    #        confFile[xdex] = x.strip('\n')
-        
-    #     xdex += 1
-    
+    print(confFile)
 
-    
+    xdex = 0
+    for x in confFile:
+        if x == "\n":
+            confFile.pop(xdex)
+        xdex += 1
+
+    xdex = 0
+    for x in confFile:
+        if "\n" in x:
+            confFile[xdex] = x.replace("\n","")
+        xdex += 1
+
+    xdex = 0
+    for x in confFile:
+        if x == "":
+            confFile.pop(xdex)
+        xdex += 1
+
+    # =========================================================
+
+    # global id
+    id = confFile[1]
+    # global secret
+    secret = confFile[3]
+    print(id)
+    print(secret)
+
+    xdex = 0
+    ginst = False
+    uinst = False
+    for x, val in enumerate(confFile):
+
+        if uinst == True:
+            playl.append(val)
+            print("Adding to playlist... " + val)
+        if val == "[Playlists]":
+            uinst = True
+            ginst = False
+
+        if ginst == True:
+            gPlayl.append(val)
+            print("Adding to global playlist... " + val)
+        if val == "[Global-Playlists]":
+            ginst = True
+
+        
+        
+    print(id)
+    print(secret)
+    print(gPlayl)
+    print(playl)
+    # updateVar()
+
+# def getID():
+#     print(id)
+#     return id
+
+# def updateVar():
+#     globals.confFile = confFile
+#     print(id)
+#     globals.id = id
+#     globals.secret = secret
+#     globals.gPlayl = gPlayl
+#     globals.playl = playl
--- Python/main.py
+++ Python/main.py
@@ -1,13 +1,29 @@
 # Smart-Playlist app
-from config import *
+
+import config
 import os.path
+
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
 print("Welcome to Smart-Playlist \nCreated by Tyler Moen\n")
 
 # Checks for config folder
 config_exists = os.path.isdir('config')
 if (config_exists == False):
-    genConfig()
+    config.genConfig()
 
-readConfig()
-print(confFile)
+config.readConfig()
+
+#config.init()
+x = config.id
+print("X equals = " + x)
+
+
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=config.id,
+                                                           client_secret=config.secret))
+
+def testRun():
+    results = sp.search(q='weezer', limit=20)
+    for idx, track in enumerate(results['tracks']['items']):
+        print(idx, track['name'])
