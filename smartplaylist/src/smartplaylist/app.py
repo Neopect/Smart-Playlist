92707cdb896c952e7c7222c774ec9a24f2cff6b3
Tyler <33147253+Neopect@users.noreply.github.com>
2021-09-30 19:29:48 -0500

    Created beeware project

---
+++ smartplaylist/src/smartplaylist/app.py
@@ -0,0 +1,27 @@
+"""
+A Spotify app that creates group playlists based on a set of user's selected playlists.
+"""
+import toga
+from toga.style import Pack
+from toga.style.pack import COLUMN, ROW
+
+
+class SmartPlaylist(toga.App):
+
+    def startup(self):
+        """
+        Construct and show the Toga application.
+
+        Usually, you would add your application to a main content box.
+        We then create a main window (with a name matching the app), and
+        show the main window.
+        """
+        main_box = toga.Box()
+
+        self.main_window = toga.MainWindow(title=self.formal_name)
+        self.main_window.content = main_box
+        self.main_window.show()
+
+
+def main():
+    return SmartPlaylist()
